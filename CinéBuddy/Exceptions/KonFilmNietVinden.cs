d60bba3850a52d015bc4740679ddb91c2029b3af
ImreLammers <imrelammers@gmail.com>
2017-01-14 16:21:15 +0100

    Added some Exceptions

---
+++ CinéBuddy/Exceptions/KonFilmNietVinden.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinéBuddy.Exceptions
+{
+    public class KonFilmNietVinden : Exception
+    {
+        public KonFilmNietVinden(string message):base(message)
+        {
+
+        }
+    }
+}
---
+++ CinéBuddy/Exceptions/KonNietMetDeDatabaseVerbinden.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinéBuddy.Exceptions
+{
+    public class KonNietMetDeDatabaseVerbinden : Exception
+    {
+        public KonNietMetDeDatabaseVerbinden(string message):base(message)
+        {
+
+        }
+    }
+}
