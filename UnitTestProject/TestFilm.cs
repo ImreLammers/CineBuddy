b3494c24542389be269029ab7a85f4a3699eaa44
ImreLammers <imrelammers@gmail.com>
2017-01-14 17:10:52 +0100

    Added and Implemented Unit Tests

---
+++ UnitTestProject/TestFilm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CinéBuddy.DB.Local;
+using CinéBuddy.DB.Repository;
+using CinéBuddy.Models.Types;
+using CinéBuddy.Models.Classes;
+using CinéBuddy.Models;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestFilm
+    {
+        private FilmRepository fr = new FilmRepository(new FilmContext());
+
+        [TestMethod]
+        public void FilmTest()
+        {
+            List<Film> allFilms = new List<Film>();
+            allFilms = fr.GetAllFilms();
+            Assert.IsTrue(allFilms.Count == 3);
+
+            Assert.IsNotNull(fr.HaalFilmOp(1));
+            Assert.IsNull(fr.HaalFilmOp(5));
+
+            Film testFilm = fr.HaalFilmOp(0);
+            Assert.AreEqual(testFilm.titel, "Memento");
+
+            Assert.AreNotEqual(fr.HaalFilmOp(1), fr.HaalFilmOp(2));
+        }
+    }
+}
---
+++ UnitTestProject/TestUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CinéBuddy.DB.Local;
+using CinéBuddy.DB.Repository;
+using CinéBuddy.Models.Types;
+using CinéBuddy.Models.Classes;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestUser
+    {
+        UserRepository ur = new UserRepository(new UserContext());
+
+        [TestMethod]
+        public void UserTest()
+        {
+            List<Gebruiker> allUsers = new List<Gebruiker>();
+            allUsers = ur.GetAllUsers();
+            Assert.IsTrue(allUsers.Count == 3);
+
+            Assert.IsNotNull(ur.GetUserByUserName("Job"));
+            Assert.IsNull(ur.GetUserByUserName("Banaan"));
+
+            Gebruiker testGebruiker = ur.GetUserByUserName("Imre");
+            Assert.IsTrue(ur.AnnuleerAbonnement(testGebruiker.gebruikersNaam));
+            Assert.AreEqual(testGebruiker.gold, 0);
+            Assert.IsTrue(ur.AbonneerAbonnement(testGebruiker.gebruikersNaam));
+            Assert.AreEqual(testGebruiker.gold, 1);
+        }
+    }
+}
